b0686a363d04e72ac08183c61b90ed1147d764d3
phuc <huyphuc0906@gmail.com>
2026-03-01 16:10:12 +0700

    - Roll the ball: exercise 001 and 002

---
+++ kotlin/kotlin/exercises/Exercise_001.kts
@@ -0,0 +1,3 @@
+// https://www.bumbii.tech/bai-tap-lap-trinh/600-bai-tap-tong-hop#basic-inputoutput--math
+println("Hello, World!")
+
---
+++ kotlin/kotlin/exercises/Exercise_002.kts
@@ -0,0 +1,9 @@
+// https://www.bumbii.tech/bai-tap-lap-trinh/600-bai-tap-tong-hop#basic-inputoutput--math
+print("Please enter a number: ")
+val input = readln()
+val intNumber = input.trim().toIntOrNull()
+if (intNumber != null) {
+    print("You entered: $intNumber")
+} else {
+    print("\"$input\" is not a number")
+}
